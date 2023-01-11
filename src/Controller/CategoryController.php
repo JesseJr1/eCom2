fd2a887620d684907f76f1e6dd26b5a78bf63464
JesseJr1 <tobias.jess@gmail.com>
2023-01-11 16:16:24 +0100

    View up On Category + Products. WIP on component.

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,10 +11,18 @@ class CategoryController extends AbstractController
 {
     #[Route('/categories', name: 'app_categories')]
     //category -> categories
-    public function index(): Response
+    public function list(CategoryRepository $categoryRepository): Response
     {
-        return $this->render('category/index.html.twig', [
-            'controller_name' => 'CategoryController',
+        return $this->render('category/list.html.twig', [
+            'categories' => $categoryRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/categories/{id}', name: 'app_category')]
+    public function show(CategoryRepository $categoryRepository, string $id): Response
+    {
+        return $this->render('category/show.html.twig', [
+            'category' => $categoryRepository->find($id),
         ]);
     }
 }
