fc2ee0e3ecdcdfd7ec9e0c3065962d5a0535c64c
JesseJr1 <tobias.jess@gmail.com>
2023-01-11 15:11:12 +0100

    Canceled and replaced

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $category = new Category();
+        $category->setTitle('All catégories');
+        $manager->persist($category);
+
+
+        $laptop = new Category();
+        $laptop->setTitle('Laptop');
+        $manager->persist($laptop);
+
+        $console = new Category();
+        $console->setTitle('Console');
+        $manager->persist($console);
+
+
+
+
+
+        $manager->flush();
+    }
+}
